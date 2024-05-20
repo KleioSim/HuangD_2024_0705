06a65ca9c6a266acd5976eab3105738fec805560
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-20 18:39:04 +0800

    implement BlockBuilder

--- TileMap/BlockBuilder.cs
+++ TileMap/BlockBuilder.cs
@@ -20,25 +20,35 @@ static class BlockBuilder
 
     private static void Build(TileMap tilemap, int size, Vector2I bluePoint)
     {
+        var blueWidth = 0.1;
         bluePoint = bluePoint * size;
 
         for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < size * 0.1; j++)
+            for (int j = 0; j < size * blueWidth; j++)
             {
                 tilemap.SetCell(0, new Vector2I(Math.Abs(bluePoint.X - i), Math.Abs(bluePoint.Y - j)), 3, new Vector2I(0, 0), 0);
                 tilemap.SetCell(0, new Vector2I(Math.Abs(bluePoint.X - j), Math.Abs(bluePoint.Y - i)), 3, new Vector2I(0, 0), 0);
             }
         }
 
-        var yellowPoint = new Vector2I(size - 1 - bluePoint.X, size - 1 - bluePoint.Y);
-
-        for (int i = 0; i < size; i++)
+        var peerPoint = new Vector2I(size - 1 - bluePoint.X, size - 1 - bluePoint.Y);
+        for (int i = 0; i < size * (1 - blueWidth); i++)
         {
-            for (int j = 0; j < size * 0.1; j++)
+            for (int j = 0; j < size * (1 - blueWidth); j++)
             {
-                tilemap.SetCell(0, new Vector2I(Math.Abs(yellowPoint.X - i), Math.Abs(yellowPoint.Y - j)), 1, new Vector2I(0, 0), 0);
-                tilemap.SetCell(0, new Vector2I(Math.Abs(yellowPoint.X - j), Math.Abs(yellowPoint.Y - i)), 1, new Vector2I(0, 0), 0);
+                tilemap.SetCell(0, new Vector2I(Math.Abs(peerPoint.X - i), Math.Abs(peerPoint.Y - j)), 0, new Vector2I(0, 0), 0);
+                tilemap.SetCell(0, new Vector2I(Math.Abs(peerPoint.X - j), Math.Abs(peerPoint.Y - i)), 0, new Vector2I(0, 0), 0);
+            }
+        }
+
+        var purpleWidth = 0.25;
+        for (int i = 0; i < size * (1 - blueWidth); i++)
+        {
+            for (int j = 0; j < size * purpleWidth; j++)
+            {
+                tilemap.SetCell(0, new Vector2I(Math.Abs(peerPoint.X - i), Math.Abs(peerPoint.Y - j)), 2, new Vector2I(0, 0), 0);
+                tilemap.SetCell(0, new Vector2I(Math.Abs(peerPoint.X - j), Math.Abs(peerPoint.Y - i)), 2, new Vector2I(0, 0), 0);
             }
         }
     }
