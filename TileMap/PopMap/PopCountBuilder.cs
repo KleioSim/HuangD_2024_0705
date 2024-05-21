4c0eca3d8c70bda18d8d4cc6c06a31fed15332e3
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-21 22:54:50 +0800

    set pop count map color

--- TileMap/PopMap/PopCountBuilder.cs
+++ TileMap/PopMap/PopCountBuilder.cs
@@ -6,11 +6,22 @@ static class PopCountBuilder
 {
     internal static void Build(TileMap popCountMap, Dictionary<Vector2I, TerrainBuilder.TileType> dict)
     {
-        foreach(var pair in dict)
+        popCountMap.Clear();
+        for (int i = 0; i < dict.Count; i++)
+        {
+            popCountMap.AddLayer(i);
+        }
+
+        popCountMap.AddLayer(1);
+
+        foreach (var pair in dict)
         {
             if(pair.Value != TerrainBuilder.TileType.Water)
             {
-                popCountMap.SetCellEx(0, pair.Key, 0);
+
+
+                popCountMap.SetCellEx(99, pair.Key, 0);
+                popCountMap.SetLayerModulate(99, new Color(1f, 0.4f, 0.4f));
             }
         }
     }
