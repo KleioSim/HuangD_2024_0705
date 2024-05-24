ade276883a6416e862502d13241082eb07af6e77
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-24 14:33:52 +0800

    Add addons/CameraMoveControl

---
+++ addons/CameraMoveControl/CameraMoveControl.cs
@@ -0,0 +1,83 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CameraMoveControl
+{
+    public partial class CameraMoveControl : Control
+    {
+        [Export]
+        Camera2D Camera { get; set; }
+
+        bool isMoveOn;
+        float moveAngle;
+
+        Control[] controlPanel => new[]
+        {
+            GetNode<Control>("Content/Top"),
+            GetNode<Control>("Content/Bottom"),
+            GetNode<Control>("Content/Right"),
+            GetNode<Control>("Content/Left"),
+        };
+
+        public override void _EnterTree()
+        {
+            var controlPanel = GD.Load<PackedScene>("res://addons/CameraMoveControl/CameraMoveControl.tscn").Instantiate();
+            this.AddChild(controlPanel, true);
+
+            var paths = new[] { "Top", "Bottom", "Right", "Left" };
+            foreach (var path in paths)
+            {
+                var control = controlPanel.GetNode<Control>(path);
+                control.GuiInput += OnControlGUIInput;
+                control.MouseEntered += OnMouseEnterControl;
+                control.MouseExited += OnMouseExitControl;
+            }
+
+            this.MouseFilter = MouseFilterEnum.Ignore;
+        }
+
+
+        public void OnMouseEnterControl()
+        {
+            isMoveOn = true;
+        }
+
+        public void OnMouseExitControl()
+        {
+            isMoveOn = false;
+        }
+
+        public void OnControlGUIInput(InputEvent @event)
+        {
+            if (!isMoveOn)
+            {
+                return;
+            }
+
+            if (@event is InputEventMouseMotion moveEvent)
+            {
+
+                moveAngle = (GlobalPosition + (Size / 2)).AngleToPoint(moveEvent.GlobalPosition);
+
+                GD.Print($"{moveAngle})");
+
+            }
+        }
+
+        public override void _Process(double delta)
+        {
+            if (!isMoveOn)
+            {
+                return;
+            }
+
+
+            Camera.Position = Camera.Position + (Vector2.Right * 10).Rotated(moveAngle);
+        }
+    }
+}
---
+++ addons/CameraMoveControl/Plugin.cs
@@ -0,0 +1,24 @@
+﻿#if TOOLS
+using Godot;
+using System;
+
+namespace CameraMoveControl
+{
+    [Tool]
+    public partial class Plugin : EditorPlugin
+    {
+        public override void _EnterTree()
+        {
+            var script = GD.Load<Script>("res://addons/CameraMoveControl/CameraMoveControl.cs");
+            var texture = GD.Load<Texture2D>("res://addons/CameraMoveControl/Icon.png");
+            AddCustomType("CameraMoveControl", "Control", script, texture);
+        }
+
+        public override void _ExitTree()
+        {
+            RemoveCustomType("CameraMoveControl");
+        }
+    }
+}
+
+#endif
