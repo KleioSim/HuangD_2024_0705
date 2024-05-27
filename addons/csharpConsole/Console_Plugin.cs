2b68be88cc66b8eb556445f907bddb65e4c50bf3
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-05-27 18:28:19 +0800

    add addons/csharpConsole

---
+++ addons/csharpConsole/Console_Plugin.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+#if TOOLS
+[Tool]
+public partial class Console_Plugin : EditorPlugin
+{
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        AddAutoloadSingleton("CommandConsole", "res://addons/csharpConsole/CommandConsole.cs");
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        RemoveAutoloadSingleton("CommandConsole");
+    }
+}
+#endif
