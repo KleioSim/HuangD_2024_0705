ad06c48e82a9fc208ce8d8e468951c7f82be0cd7
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-11 10:25:17 +0800

    Add New EventSystem

--- HuangD.Modders.Native/EventDef.cs
+++ HuangD.Modders.Native/EventDef.cs
@@ -7,43 +7,16 @@ using HuangD.Sessions.Interfaces;
 namespace HuangD.Modders.Native;
 
 [DefTo(typeof(Country))]
-public class EventDef : IEventDef
+public class EventDef2 : IEventDef
 {
-    public ICondition Condition { get; } = new TrueCondtion();
-
-    public ITargetFinder TargetFinder { get; } = new TargetFinder()
-    {
-        Targets = new NeighorCountires(),
-        ConditionFactors = new ICondtionFactor[]
-        {
-            new CondtionFactor()
-            {
-                Condition = new TrueCondtion(),
-                Factor = 0.3
-            }
-        }
-    };
-
     public IOption Option { get; } = new Option()
     {
-        MessageBinds = new IMessageBind[]
-        {
-            new MessageBind()
-            {
-                MessageType = typeof(Message_Start),
-                TargetVisitor = new EventTargetVisitor(),
-                ValueVisitor = new EventFromVisitor()
-            }
-        }
-    };
-}
-
-[DefTo(typeof(Country))]
-public class EventDef2 : IEventDef2
-{
-    public IOption2 Option { get; } = new Option2()
-    {
         Desc = "Test",
+
+        ProductMessage = (entity, to, session) =>
+        {
+            return new[] { new Message_Start() { Target = entity, Value = to } };
+        }
     };
 
     private Random random = new Random();
@@ -66,15 +39,5 @@ public class EventDef2 : IEventDef2
     public bool IsSatisfied(IEntity entity, ISession session)
     {
         return true;
-    }
-}
-
-internal class Option2 : IOption2
-{
-    public string Desc { get; init; }
-
-    public IEnumerable<IMessage> Do(IEntity entity, ISession session)
-    {
-        throw new NotImplementedException();
     }
 }
