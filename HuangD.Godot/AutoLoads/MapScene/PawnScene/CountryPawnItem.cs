f433550231db4f3f8ab9a8a7906d3853d3ae0c75
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-24 18:42:57 +0800

    solve isuess

--- HuangD.Godot/AutoLoads/MapScene/PawnScene/CountryPawnItem.cs
+++ HuangD.Godot/AutoLoads/MapScene/PawnScene/CountryPawnItem.cs
@@ -7,20 +7,32 @@ public partial class CountryPawnItem : ViewControl, IItemView
 {
     public static Func<Country, Vector2> GetPawnPosition;
 
-    public object Id { get; set; }
+    public object Id
+    {
+        get => id;
+        set
+        {
+            if (id != value)
+            {
+                id = value;
+                this.Position = GetPawnPosition(Country);
+            }
+        }
+    }
+
+    private object id;
 
     private Country Country => Id as Country;
     private Label Label => GetNode<Label>("Label");
 
     protected override void Initialize()
     {
-        Label.Text = Country.Name;
-        this.Position = GetPawnPosition(Country);
+
     }
 
     protected override void Update()
     {
         Label.Text = Country.Name;
-        this.Position = GetPawnPosition(Country);
+        this.Name = Country.Name;
     }
 }
--- HuangD.Godot/AutoLoads/MapScene/PawnScene/ProvincePawnItem.cs
+++ HuangD.Godot/AutoLoads/MapScene/PawnScene/ProvincePawnItem.cs
@@ -8,20 +8,32 @@ public partial class ProvincePawnItem : ViewControl, IItemView
 {
     public static Func<Province, Vector2> GetPawnPosition;
 
-    public object Id { get; set; }
+    public object Id
+    {
+        get => id;
+        set
+        {
+            if (id != value)
+            {
+                id = value;
+                this.Position = GetPawnPosition(Province);
+            }
+        }
+    }
+
+    private object id;
 
     private Province Province => Id as Province;
     private Label Label => GetNode<Label>("Label");
 
     protected override void Initialize()
     {
-        Label.Text = Province.Name;
-        this.Position = GetPawnPosition(Province);
+
     }
 
     protected override void Update()
     {
         Label.Text = Province.Name;
-        this.Position = GetPawnPosition(Province);
+        this.Name = Province.Name;
     }
 }
