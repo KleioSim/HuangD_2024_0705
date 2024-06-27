065d98e3e4664d45eea9c52fadd7034dc84d8f18
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-06-27 17:56:05 +0800

    Add PopCountBuilder

--- HuangD.Godot/TileMap/PopMap/TileMapPopCount.cs
+++ HuangD.Godot/TileMap/PopMap/TileMapPopCount.cs
@@ -1,6 +1,28 @@
-using Godot;
+﻿using Godot;
 using System;
+using System.Linq;
 
 public partial class TileMapPopCount : TileMap
 {
+    public override void _Ready()
+    {
+        var terrainDict = HuangD.Sessions.Map.TerrainBuilder.Build(64, "Test");
+        GD.Print(string.Join(",", terrainDict.Values.GroupBy(x => x).Select(g => $"{g.Key}:{g.Count()}")));
+
+        var popDict = HuangD.Sessions.Map.PopCountBuilder.Build(terrainDict, "test");
+        GD.Print($"total pop count {popDict.Values.Sum()}, max {popDict.Values.Max()}, min {popDict.Values.Min()}");
+
+        for (int i = 0; i < 10; i++)
+        {
+            this.AddLayer(i);
+            this.SetLayerModulate(i, new Color(1f, (10 - i) * 0.1f, (10 - i) * 0.1f));
+        }
+
+        var max = popDict.Max(x => x.Value);
+        var min = popDict.Min(x => x.Value);
+        foreach (var pair in popDict)
+        {
+            this.SetCellEx(Math.Min(pair.Value / 1000, 10), new Vector2I(pair.Key.X, pair.Key.Y), 0);
+        }
+    }
 }
--- HuangD.Sessions/Session.cs
+++ HuangD.Sessions/Session.cs
@@ -6,6 +6,7 @@ using HuangD.Sessions.Interfaces;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using static HuangD.Sessions.Map;
 
@@ -634,6 +635,67 @@ public class Map
                 }
 
             }
+        }
+    }
+
+    public static class PopCountBuilder
+    {
+        public static Dictionary<Index, int> Build(Dictionary<Index, TerrainType> terrainDict, string seed)
+        {
+            var random = new Random();
+            var baseValueDict = terrainDict.Where(x => x.Value != TerrainType.Water).ToDictionary(k => k.Key, v =>
+            {
+                int popCount = 0;
+
+                var index = v.Key;
+                var terrain = v.Value;
+
+                switch (terrain)
+                {
+                    case TerrainType.Land:
+                        popCount = random.Next(500, 1000);
+                        break;
+                    case TerrainType.Hill:
+                        popCount = random.Next(100, 200);
+                        break;
+                    case TerrainType.Mount:
+                        popCount = random.Next(0, 50);
+                        break;
+                }
+
+
+                popCount += Map.IndexMethods.GetNeighborCells(index).Values.Select(neighbor =>
+                {
+                    if (terrainDict.TryGetValue(neighbor, out TerrainType neighborTerrain))
+                    {
+                        switch (neighborTerrain)
+                        {
+                            case TerrainType.Land:
+                                return random.Next(100, 500);
+                            case TerrainType.Hill:
+                                return random.Next(0, 100);
+                            case TerrainType.Mount:
+                                return 0;
+                        }
+                    }
+
+                    return 0;
+                }).Sum();
+
+                return popCount;
+            });
+
+            return baseValueDict.ToDictionary(k => k.Key, v =>
+            {
+                var currIndex = v.Key;
+                var currValue = v.Value;
+
+                var neighorValues = Map.IndexMethods.GetNeighborCells(currIndex).Values
+                    .Where(neighbor => baseValueDict.ContainsKey(neighbor))
+                    .Select(neighbor => baseValueDict[neighbor]);
+
+                return currValue * Math.Min(1, (int)(neighorValues.Average()) / 1000);
+            });
         }
     }
 }
